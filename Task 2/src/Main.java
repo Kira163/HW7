facb1a8fedf277c3a8281714bb0b63d43be878f7
Dmitry <dmitrykira@ya.ru>
2024-06-26 11:50:10 +0400

    сделал список увольняемых

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         //todo Тут увольняем айтишников
-    }
-}
+                int totalITWorkers = 100;
+                int id = 1;
+
+                while (id <= totalITWorkers) {
+                    if (id % 2 == 0) {
+                        System.out.println("Уволен айтишник с id " + id);
+                    }
+                    id++;
+                }
+            }
+        }
+
+
